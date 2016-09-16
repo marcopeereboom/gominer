80ffd0575c2ca64ce43fff97345df681e7a64351
Josh Rickmar <jrick@companyzero.com>
2016-09-16 12:51:19 -0400

    only load the dll from windows. untested

---
+++ cudakernel_static.go
@@ -0,0 +1,34 @@
+// Copyright (c) 2016 The Decred developers.
+
+//+build linux darwin
+
+package main
+
+/*
+#include <stdint.h>
+void decred_hash_nonce(uint32_t grid, uint32_t block, uint32_t threads, uint32_t startNonce, uint32_t *resNonce, uint32_t targetHigh);
+void decred_cpu_setBlock_52(const uint32_t *input);
+*/
+import "C"
+import (
+	"github.com/mumax/3/cuda/cu"
+	"unsafe"
+)
+
+var (
+	kernelDll           = syscall.MustLoadDLL("decred.dll")
+	precomputeTableProc = kernelDll.MustFindProc("decred_cpu_setBlock_52")
+	kernelProc          = kernelDll.MustFindProc("decred_hash_nonce")
+)
+
+func cudaPrecomputeTable(input *[192]byte) {
+	if input == nil {
+		panic("input is nil")
+	}
+	C.decred_cpu_setBlock_52((*C.uint32_t)(unsafe.Pointer(input)))
+}
+
+func cudaInvokeKernel(gridx, blockx, threads uint32, startNonce uint32, nonceResults cu.DevicePtr, targetHigh uint32) {
+	C.decred_hash_nonce(C.uint32_t(gridx), C.uint32_t(blockx), C.uint32_t(threads),
+		C.uint32_t(startNonce), (*C.uint32_t)(unsafe.Pointer(nonceResults)), C.uint32_t(targetHigh))
+}
--- cudevice.go
+++ cudevice.go
@@ -2,12 +2,6 @@
 
 package main
 
-/*
-#include <stdint.h>
-void decred_hash_nonce(uint32_t grid, uint32_t block, uint32_t threads, uint32_t startNonce, uint32_t *resNonce, uint32_t targetHigh);
-void decred_cpu_setBlock_52(const uint32_t *input);
-*/
-import "C"
 import (
 	"encoding/binary"
 	"fmt"
@@ -29,48 +23,36 @@ const (
 	blockx          = threadsPerBlock
 )
 
-func decredCPUSetBlock52(input *[192]byte) {
-	if input == nil {
-		panic("input is nil")
-	}
-	C.decred_cpu_setBlock_52((*C.uint32_t)(unsafe.Pointer(input)))
-}
-
-func decredHashNonce(gridx, blockx, threads uint32, startNonce uint32, nonceResults cu.DevicePtr, targetHigh uint32) {
-	C.decred_hash_nonce(C.uint32_t(gridx), C.uint32_t(blockx), C.uint32_t(threads),
-		C.uint32_t(startNonce), (*C.uint32_t)(unsafe.Pointer(nonceResults)), C.uint32_t(targetHigh))
-}
-
 func deviceInfoNVIDIA(index int) (uint32, uint32) {
 	fanPercent := uint32(0)
 	temperature := uint32(0)
 
-//	err := nvml.Init()
-//	if err != nil {
-//		minrLog.Errorf("NVML Init error: %v", err)
-//		return fanPercent, temperature
-//	}
-//
-//	dh, err := nvml.DeviceGetHandleByIndex(index)
-//	if err != nil {
-//		minrLog.Errorf("NVML DeviceGetHandleByIndex error: %v", err)
-//		return fanPercent, temperature
-//	}
-//
-//	nvmlFanSpeed, err := nvml.DeviceFanSpeed(dh)
-//	if err != nil {
-//		minrLog.Infof("NVML DeviceFanSpeed error: %v", err)
-//	} else {
-//		fanPercent = uint32(nvmlFanSpeed)
-//	}
-//
-//	nvmlTemp, err := nvml.DeviceTemperature(dh)
-//	if err != nil {
-//		minrLog.Infof("NVML DeviceTemperature error: %v", err)
-//	} else {
-//		temperature = uint32(nvmlTemp)
-//	}
-//
+	//	err := nvml.Init()
+	//	if err != nil {
+	//		minrLog.Errorf("NVML Init error: %v", err)
+	//		return fanPercent, temperature
+	//	}
+	//
+	//	dh, err := nvml.DeviceGetHandleByIndex(index)
+	//	if err != nil {
+	//		minrLog.Errorf("NVML DeviceGetHandleByIndex error: %v", err)
+	//		return fanPercent, temperature
+	//	}
+	//
+	//	nvmlFanSpeed, err := nvml.DeviceFanSpeed(dh)
+	//	if err != nil {
+	//		minrLog.Infof("NVML DeviceFanSpeed error: %v", err)
+	//	} else {
+	//		fanPercent = uint32(nvmlFanSpeed)
+	//	}
+	//
+	//	nvmlTemp, err := nvml.DeviceTemperature(dh)
+	//	if err != nil {
+	//		minrLog.Infof("NVML DeviceTemperature error: %v", err)
+	//	} else {
+	//		temperature = uint32(nvmlTemp)
+	//	}
+	//
 	return fanPercent, temperature
 }
 
@@ -222,7 +204,7 @@ func (d *Device) runCuDevice() error {
 			i += 4
 			j++
 		}
-		decredCPUSetBlock52(endianData)
+		cudaPrecomputeTable(endianData)
 
 		// Update the timestamp. Only solo work allows you to roll
 		// the timestamp.
@@ -249,7 +231,7 @@ func (d *Device) runCuDevice() error {
 
 		targetHigh := ^uint32(0)
 
-		decredHashNonce(gridx, blockx, throughput, startNonce, nonceResultsD, targetHigh)
+		cudaInvokeKernel(gridx, blockx, throughput, startNonce, nonceResultsD, targetHigh)
 
 		cu.MemcpyDtoH(nonceResultsH, nonceResultsD, d.cuInSize)
 
