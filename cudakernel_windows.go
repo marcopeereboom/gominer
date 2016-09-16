6f0f9b916dea72e68ec2b43dbc6d7b26ab797ab1
Josh Rickmar <jrick@companyzero.com>
2016-09-16 15:46:45 -0400

    optimize

--- cudakernel_windows.go
+++ cudakernel_windows.go
@@ -11,16 +11,16 @@ import (
 
 var (
 	//kernelDll           = syscall.MustLoadDLL("decred.dll")
-	kernelDll           = syscall.MustLoadDLL("obj/decred.dll")
-	precomputeTableProc = kernelDll.MustFindProc("decred_cpu_setBlock_52")
-	kernelProc          = kernelDll.MustFindProc("decred_hash_nonce")
+	kernelDll               = syscall.MustLoadDLL("obj/decred.dll")
+	precomputeTableProcAddr = kernelDll.MustFindProc("decred_cpu_setBlock_52").Addr()
+	kernelProcAddr          = kernelDll.MustFindProc("decred_hash_nonce").Addr()
 )
 
 func cudaPrecomputeTable(input *[192]byte) {
-	precomputeTableProc.Call(uintptr(unsafe.Pointer(input)))
+	syscall.Syscall(precomputeTableProcAddr, 1, uintptr(unsafe.Pointer(input)))
 }
 
 func cudaInvokeKernel(gridx, blockx, threads uint32, startNonce uint32, nonceResults cu.DevicePtr, targetHigh uint32) {
-	kernelProc.Call(uintptr(gridx), uintptr(blockx), uintptr(threads), uintptr(startNonce),
-		uintptr(nonceResults), uintptr(targetHigh))
+	syscall.Syscall6(kernelProcAddr, 6, uintptr(gridx), uintptr(blockx), uintptr(threads),
+		uintptr(startNonce), uintptr(nonceResults), uintptr(targetHigh))
 }
